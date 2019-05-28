d6d96dfe37c411a288c6fa53ad707e52f2863bfa
ASHPLAN MEDIA <ashplanmedia@gmail.com>
2019-05-28 10:43:25 +0530

    First Commit

---
+++ application/views/form.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+	<head>
+		  <link rel="stylesheet" href="stylesheet1.css">
+		</head>
+<div class="login">
+    <input type="text" placeholder="Username" id="username">  
+  <input type="password" placeholder="password" id="password">  
+  <a href="#" class="forgot">forgot password?</a>
+  <input type="submit" value="Sign In">
+</div>
+<div class="shadow"></div>
+</body>
+</html>
